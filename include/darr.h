5691b6837b1418e54ea5138240fd5a75459c819b
Nobuharu Shimazu <nobu.bichanna@gmail.com>
2024-07-01 17:09:14 -0400

    Fix darr.h

--- include/darr.h
+++ include/darr.h
@@ -5,38 +5,58 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+// A very simple dynamic array implementation using a big macro.
+//
+// Usage:
+//
+// MAKE_DARR(int);
+// DArr_int *da = darr_int_create();
+// darr_int_push(da, 123);
+// darr_int_push(da, 321);
+//
+// for (int i = 0; i < da->count; i++) {
+//   printf("%d\n", darr_int_get(da, i));
+// }
+//
+// darr_int_destroy(da);
+
 #define DARR_INIT_SIZE 64
 
-#define MAKE_DARR(TYPE)                                    \
-  typedef struct {                                         \
-    TYPE *array;                                           \
-    size_t count;                                          \
-    size_t capacity;                                       \
-  } DArr_##TYPE;                                           \
-  DArr_##TYPE darr_##TYPE_create() {                       \
-    DArr_##TYPE *da = malloc(sizeof(DArr_##TYPE));         \
-    da->count = 0;                                         \
-    da->capacity = DARR_INIT_SIZE;                         \
-    da->array = malloc(DARR_INIT_SIZE * sizeof(TYPE));     \
-  }                                                        \
-  void darr_##TYPE_push(DArr_##TYPE *da, TYPE elem) {      \
-    if (++(da->count) == da->capacity) {                   \
-      da->capacity *= 2;                                   \
-      da->array = realloc(da->array, da->capacity * TYPE); \
-    }                                                      \
-    da->array[da->count - 1] = elem;                       \
-  }                                                        \
-  ##TYPE *darr_##TYPE_pop(DArr_##TYPE *da) {               \
-    if (da->count == 0) return NULL;                       \
-    return &(da->array[--(da->count)]);                    \
-  }                                                        \
-  ##TYPE *darr_##TYPE_get(DArr_##TYPE *da, size_t idx) {   \
-    if (da->count <= idx) return NULL;                     \
-    return &(da->array[idx]);                              \
-  }                                                        \
-  void darr_##TYPE_destroy(DArr_##TYPE *da) {              \
-    free(da->array);                                       \
-    free(da);                                              \
+#define MAKE_DARR(TYPE)                                          \
+  typedef struct {                                               \
+    TYPE *array;                                                 \
+    size_t count;                                                \
+    size_t capacity;                                             \
+  } DArr_##TYPE;                                                 \
+  DArr_##TYPE *darr_##TYPE_create() {                            \
+    DArr_##TYPE *da = (DArr_##Type)malloc(sizeof(DArr_##TYPE));  \
+    if (da == NULL) return NULL;                                 \
+    da->count = 0;                                               \
+    da->capacity = DARR_INIT_SIZE;                               \
+    da->array = (##Type *)malloc(DARR_INIT_SIZE * sizeof(TYPE)); \
+    if (da->array == NULL) {                                     \
+      free(da);                                                  \
+      return NULL;                                               \
+    }                                                            \
+  }                                                              \
+  void darr_##TYPE_push(DArr_##TYPE *da, TYPE elem) {            \
+    if (++(da->count) == da->capacity) {                         \
+      da->capacity *= 2;                                         \
+      da->array = realloc(da->array, da->capacity * TYPE);       \
+    }                                                            \
+    da->array[da->count - 1] = elem;                             \
+  }                                                              \
+  ##TYPE *darr_##TYPE_pop(DArr_##TYPE *da) {                     \
+    if (da->count == 0) return NULL;                             \
+    return &(da->array[--(da->count)]);                          \
+  }                                                              \
+  ##TYPE *darr_##TYPE_get(DArr_##TYPE *da, size_t idx) {         \
+    if (da->count <= idx) return NULL;                           \
+    \ return &(da->array[idx]);                                  \
+  }                                                              \
+  void darr_##TYPE_destroy(DArr_##TYPE *da) {                    \
+    free(da->array);                                             \
+    free(da);                                                    \
   }
 
 #endif
